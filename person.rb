4c914b03e875ea527b3f72359e03bdd759cfcd39
Hack Reactor Students <github@hackreactor.com>
2013-07-16 18:58:37 -0700

    white-space cleaning

--- person.rb
+++ person.rb
@@ -6,37 +6,37 @@
 
 class Person
 
-	# class variable initialization
-	@@population = 0
+  # class variable initialization
+  @@population = 0
 
-	# initialization method
-	# that gets called with new()
-	def initialize(a, n)
+  # initialization method
+  # that gets called with new()
+  def initialize(a, n)
 
-		# instance variables
-		@age = a
-		@first_name = n
+    # instance variables
+    @age = a
+    @first_name = n
 
-		#increment class variable
-		@@population += 1
-	end
+    #increment class variable
+    @@population += 1
+  end
 
-	# instance methods to get and set
-	# instance variables
-	def age
-		@age
-	end
+  # instance methods to get and set
+  # instance variables
+  def age
+    @age
+  end
 
-	def first_name
-		@first_name
-	end
+  def first_name
+    @first_name
+  end
 
 
-	# class level method to get and set
-	# class variables
-	def self.population
-		@@population
-	end
+  # class level method to get and set
+  # class variables
+  def self.population
+    @@population
+  end
 end
 
 ############### Example use  #####################
@@ -44,16 +44,16 @@ end
 
 jon = Person.new(23, "Jon")
 
-jon.age;	
+jon.age;  
 # => 23
 
-jon.first_name;	
+jon.first_name; 
 # => "Jon"
 
-jon.population		
+jon.population    
 # => NoMethodError: undefined method 'population' ...
 
-Person.population	
+Person.population 
 # => 1 
 
 jon.age = 50
@@ -64,11 +64,11 @@ jon.age
 mary = Person.new 35, "Mary" 
 # parenthesis optional for methods
 
-Person.population            
+Person.population
 # => 2
 
-Person.class	
+Person.class  
 # => Class
 
-jon.class 		
+jon.class     
 # => Person
